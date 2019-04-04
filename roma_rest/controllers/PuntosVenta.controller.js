a9dd71a8d97a16e0dd79fe4c23141b5ac7610fcf
erojas91 <esteban.j.rojas50@gmail.com>
2019-04-04 17:24:30 -0300

    Actualizacion y eliminacion de puntos de venta. Funcionando

--- roma_rest/controllers/PuntosVenta.controller.js
+++ roma_rest/controllers/PuntosVenta.controller.js
@@ -85,6 +85,72 @@ exports.getPuntosVentaBusqueda = function (req, res) {
 };
 
 
+exports.getDatosPuntosVenta = function (req, res) {
+    try {
+        var respuesta = JSON.stringify({ "mensaje": "La funcion no responde" });
+        var pool = new Pool({
+            connectionString: connectionString,
+        });
+
+        try {
+            (async () => {
+                respuesta = await pool.query(`
+                SELECT *
+                FROM roma.puntos_venta pv            
+                WHERE pv.id = `+ req.params.puntos_venta_id + ` `)
+                    .then(resp => {
+                        console.log(JSON.stringify(resp.rows));
+                        res.status(200).send(JSON.stringify(resp.rows));
+                    }).catch(err => {
+                        console.error("ERROR", err.stack);
+                        res.status(400).send(JSON.stringify({ "mensaje": "Sin resultados de la consulta" }));
+                    });
+                return respuesta;
+
+            })()
+
+        } catch (error) {
+            res.status(400).send(JSON.stringify({ "mensaje": error.stack }));
+        }
+
+    } catch (err) {
+        res.status(400).send("{'mensaje': 'Ocurrio un Error'}");
+    }
+};
+
+
+exports.getCaracteristicasPuntosVenta = function (req, res) {
+    try {
+        var respuesta = JSON.stringify({ "mensaje": "La funcion no responde" });
+        var pool = new Pool({
+            connectionString: connectionString,
+        });
+
+        try {
+            (async () => {
+                respuesta = await pool.query(`
+                SELECT *, gdt(6, tipo_comprobante) as descripcion 
+                FROM roma.puntos_venta_tipo_comprobantes             
+                WHERE puntos_venta_id = `+ req.params.puntos_venta_id + ` `)
+                    .then(resp => {
+                        console.log(JSON.stringify(resp.rows));
+                        res.status(200).send(JSON.stringify(resp.rows));
+                    }).catch(err => {
+                        console.error("ERROR", err.stack);
+                        res.status(400).send(JSON.stringify({ "mensaje": "Sin resultados de la consulta" }));
+                    });
+                return respuesta;
+
+            })()
+
+        } catch (error) {
+            res.status(400).send(JSON.stringify({ "mensaje": error.stack }));
+        }
+
+    } catch (err) {
+        res.status(400).send("{'mensaje': 'Ocurrio un Error'}");
+    }
+};
 
 
 exports.insertPuntoVentaReturnId = function (req, res) {
@@ -173,32 +239,19 @@ exports.actualizarDatosPuntoVenta = function (req, res) {
         const client = await pool.connect()
         try {
 
-            let codigo = (req.body.codigo != undefined) ? req.body.codigo : `null`;
-            let nombre = (req.body.nombre_producto != undefined) ? `'` + req.body.nombre_producto + `'` : `null`;
-            let descripcion = (req.body.descripcion_producto != undefined) ? `'` + req.body.descripcion_producto + `'` : `null`;
-            let descripcion_factura = (req.body.descripcion_factura != undefined) ? `'` + req.body.descripcion_factura + `'` : `null`;
-            let tipo_producto = (req.body.tipo != undefined) ? req.body.tipo : `null`;
+            let numero = (req.body.numero != undefined) ? req.body.numero : `null`;
+            let nombre_usuario = (req.body.nombre_usuario != undefined) ? `'` + req.body.nombre_usuario + `'` : `null`;
+            let sucursal = (req.body.sucursal != undefined) ? req.body.sucursal : `null`;
+            let fecha_alta = (req.body.fecha_alta != undefined) ? `'` + req.body.fecha_alta + `'` : `now()::date`;
 
             await client.query('BEGIN')
             const { rows } = await client.query(`
-            UPDATE roma.productos
+            UPDATE roma.puntos_venta
             SET 
-                codigo= `+ codigo + `, 
-                nombre= `+ nombre + `, 
-                descripcion= `+ descripcion + `, 
-                descripcion_factura= `+ descripcion_factura + `, 
-                tipo_producto= `+ tipo_producto + `, 
-                fecha_desde= now()::date 
-            WHERE id ='`+ req.body.id_producto + `' returning id`)
-
-
-            const { precios_productos } = await client.query(`
-            UPDATE roma.precios_productos
-            SET 
-                monto= `+ req.body.precio + `, 
-                unidad= `+ req.body.unidad + `, 
-                fecha_desde= now()::date
-            WHERE productos_id = '`+ req.body.id_producto + `'`)
+                numero= `+ numero + `, 
+                fecha_alta= `+ fecha_alta + `, 
+                sucursal= `+ sucursal + ` 
+            WHERE id ='`+ req.body.id_punto_venta + `' returning id`)
 
             await client.query('COMMIT')
             res.status(200).send({ "mensaje": "El Producto fue actualizado exitosamente", "id": rows[0].id });
@@ -224,8 +277,8 @@ exports.eliminarCaracteristicasPuntoVenta = function (req, res) {
             await client.query('BEGIN')
 
             await client.query(`
-            DELETE FROM roma.productos_caracteristicas 
-            WHERE productos_id = `+ req.params.productos_id + ``)
+            DELETE FROM roma.puntos_venta_tipo_comprobantes
+            WHERE puntos_venta_id = `+ req.params.punto_venta_id + ``)
 
             await client.query('COMMIT')
             res.status(200).send({ "mensaje": "Las imagenes se eliminaron exitosamente" });
--- roma_rest/routes/PuntosVentaRoutes.js
+++ roma_rest/routes/PuntosVentaRoutes.js
@@ -6,6 +6,8 @@ const middleware = require("../utillities/jwtValidaciones");
 
 router.route('/getPuntosVentaTodos/').get(middleware.checkToken, Ctrl.getPuntosVentaTodos);
 router.route('/getPuntosVentaBusqueda/:texto_busqueda').get(middleware.checkToken, Ctrl.getPuntosVentaBusqueda);
+router.route('/getDatosPuntosVenta/:puntos_venta_id').get(middleware.checkToken, Ctrl.getDatosPuntosVenta);
+router.route('/getCaracteristicasPuntosVenta/:puntos_venta_id').get(middleware.checkToken, Ctrl.getCaracteristicasPuntosVenta);
 router.route('/insertPuntoVentaReturnId/').post(middleware.checkToken, Ctrl.insertPuntoVentaReturnId);
 router.route('/insertCaracteristicasPuntoVenta/').post(middleware.checkToken, Ctrl.insertCaracteristicasPuntoVenta);
 router.route('/actualizarDatosPuntoVenta').put(Ctrl.actualizarDatosPuntoVenta);
