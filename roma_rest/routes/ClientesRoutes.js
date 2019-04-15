9336d31514c1620eb185ed0a085e70fcd62e6d76
erojas91 <esteban.j.rojas50@gmail.com>
2019-04-15 17:47:54 -0300

    Alta cliente y metodo para obtener datos del mismo para modificación.

--- roma_rest/routes/ClientesRoutes.js
+++ roma_rest/routes/ClientesRoutes.js
@@ -5,4 +5,7 @@ var Ctrl = require("../controllers/Clientes.controller");
 router.route('/insertClientePersonaDomicilio/').post(Ctrl.insertClientePersonaDomicilio);
 router.route('/getClientesTodos/').get(Ctrl.getClientesTodos);
 router.route('/getClientesBusqueda/:texto_busqueda').get(Ctrl.getClientesBusqueda);
+router.route('/getDatosClientePorId/:id').get(Ctrl.getDatosClientePorId);
+router.route('/insertClientePersonaDomicilio/').post(Ctrl.insertClientePersonaDomicilio);
+
 module.exports = router;
